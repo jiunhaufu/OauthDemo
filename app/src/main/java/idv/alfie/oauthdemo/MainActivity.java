d2c9baf3d8077264b0e1895ed6e89a36adb62d84
傅俊皓 <alfie.fu@toplogis.com>
2018-01-11 19:34:21 +0800

    整合FB GOOGLE登入資料

--- app/src/main/java/idv/alfie/oauthdemo/MainActivity.java
+++ app/src/main/java/idv/alfie/oauthdemo/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.pm.Signature;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Base64;
 import android.util.Log;
 import android.view.View;
@@ -14,21 +15,20 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
-import com.facebook.appevents.AppEventsLogger;
+import com.facebook.Profile;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
-import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -36,8 +36,6 @@ import com.google.android.gms.tasks.Task;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
@@ -49,11 +47,20 @@ public class MainActivity extends AppCompatActivity {
     private GoogleSignInClient mGoogleSignInClient;
     /*facebook*/
     private CallbackManager mCallbackManager;
+    /*UI*/
+    private TextView tv_name;
+    private TextView tv_email;
+    private TextView tv_oauthId;
+    private ImageView iv_picture;
+    /*type*/
+    private String oauth = "GUEST";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*UI*/
+        findViews();
         /*google*/
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestId()
@@ -61,133 +68,103 @@ public class MainActivity extends AppCompatActivity {
                 .requestProfile()
                 .build();
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-        //自訂登入按鈕
-        Button gSignIn = (Button)findViewById(R.id.btgin);
-        gSignIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                signIn();
-            }
-        });
-        //自訂登出按鈕
-        Button gSignOut = (Button)findViewById(R.id.btgout);
-        gSignOut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                signOut();
-            }
-        });
-
         /*facebook*/
         //printhashkey();
-
         FacebookSdk.sdkInitialize(this.getApplicationContext());
-
         mCallbackManager = CallbackManager.Factory.create();
-        LoginManager.getInstance().registerCallback(
-                mCallbackManager,
-                new FacebookCallback < LoginResult > () {
-                    @Override
-                    public void onSuccess(LoginResult loginResult) {
-                        // Handle success
-                        System.out.println("onSuccess");
-
-                        String accessToken = loginResult.getAccessToken()
-                                .getToken();
-                        Log.i("accessToken", accessToken);
-
-                        GraphRequest request = GraphRequest.newMeRequest(
-                                loginResult.getAccessToken(),
-                                new GraphRequest.GraphJSONObjectCallback() {@Override
-                                public void onCompleted(JSONObject object,
-                                                        GraphResponse response) {
-
-                                    Log.i("LoginActivity",
-                                            object.toString());
-                                    try {
-                                        String id = object.getString("id");
-                                        try {
-                                            URL profile_pic = new URL(
-                                                    "http://graph.facebook.com/" + id + "/picture?type=large");
-                                            Log.i("profile_pic",
-                                                    profile_pic + "");
-
-                                        } catch (MalformedURLException e) {
-                                            e.printStackTrace();
-                                        }
-                                        String fields = object.getString("fields");
-                                        String name = object.getString("name");
-                                        String email = object.getString("email");
-                                        String gender = object.getString("gender");
-                                        String birthday = object.getString("birthday");
-                                    } catch (JSONException e) {
-                                        e.printStackTrace();
-                                    }
-                                }
-                                });
-                        Bundle parameters = new Bundle();
-                        parameters.putString("fields","id,name,email,gender,birthday");
-                        request.setParameters(parameters);
-                        request.executeAsync();
-                    }
-
-                    @Override
-                    public void onCancel() {
-                    }
-
-                    @Override
-                    public void onError(FacebookException exception) {
-                    }
+        LoginManager.getInstance().registerCallback(mCallbackManager,
+            new FacebookCallback < LoginResult > () {
+                @Override
+                public void onSuccess(LoginResult loginResult) {
+                    updateFacebookInfo(loginResult.getAccessToken());
                 }
+                @Override
+                public void onCancel() {
+                }
+                @Override
+                public void onError(FacebookException exception) {
+                }
+            }
         );
-        //自訂登入按鈕
-        Button fSignIn = (Button)findViewById(R.id.btfin);
-        fSignIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                LoginManager.getInstance().logInWithReadPermissions(
-                        MainActivity.this,
-                        Arrays.asList("user_friends", "email", "public_profile")
-                );
-            }
-        });
-        //自訂登出按鈕
-        Button fSignOut = (Button)findViewById(R.id.btfout);
-        fSignOut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                LoginManager.getInstance().logOut();
-            }
-        });
-
-
-
     }
 
+    private void findViews() {
+        tv_name = (TextView)findViewById(R.id.tvName);
+        tv_email = (TextView)findViewById(R.id.tvEmail);
+        tv_oauthId = (TextView)findViewById(R.id.tvOauthId);
+        iv_picture = (ImageView)findViewById(R.id.ivPicture);
+
+        //google登入按鈕
+        Button btGoogleSignin = (Button)findViewById(R.id.btgin);
+        btGoogleSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                googleSignIn();
+            }
+        });
+        //google登出按鈕
+        Button btGoogleSignout = (Button)findViewById(R.id.btgout);
+        btGoogleSignout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                googleSignOut();
+            }
+        });
+
+        //facebook登入按鈕
+        Button btFacebookSignin = (Button)findViewById(R.id.btfin);
+        btFacebookSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                facebookSignIn();
+            }
+        });
+        //facebook登出按鈕
+        Button btfacebookSignout = (Button)findViewById(R.id.btfout);
+        btfacebookSignout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                facebookSignOut();
+            }
+        });
+    }
 
     @Override
     protected void onStart() {
         /*google*/
         GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-        updateUI(account);
+        updateGoogleInfo(account);
+        /*facebook*/
+        AccessToken accessToken = AccessToken.getCurrentAccessToken();
+        updateFacebookInfo(accessToken);
         super.onStart();
     }
 
     /*google*/
-    private void signIn() {
+    private void googleSignIn() {
         Intent signInIntent = mGoogleSignInClient.getSignInIntent();
         startActivityForResult(signInIntent, RC_SIGN_IN);
     }
     /*google*/
-    private void signOut() {
+    private void googleSignOut() {
         mGoogleSignInClient.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
             @Override
             public void onComplete(@NonNull Task<Void> task) {
-                updateUI(null);
+                updateGoogleInfo(null);
             }
         });
     }
-
+    /*facebook*/
+    private void facebookSignIn() {
+        LoginManager.getInstance().logInWithReadPermissions(MainActivity.this,
+                Arrays.asList("public_profile","email", "user_friends" )
+        );
+    }
+    /*facebook*/
+    private void facebookSignOut() {
+        LoginManager.getInstance().logOut();
+        updateFacebookInfo(null);
+    }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -195,57 +172,82 @@ public class MainActivity extends AppCompatActivity {
         /*google*/
         if (requestCode == RC_SIGN_IN) {
             //登入成功後取得資料
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            handleSignInResult(task);
+            try {
+                GoogleSignInAccount account = GoogleSignIn.getSignedInAccountFromIntent(data).getResult(ApiException.class);
+                //登入成功
+                updateGoogleInfo(account);
+            } catch (ApiException e) {
+                Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
+                //登入失敗
+                updateGoogleInfo(null);
+            }
         }
+        /*facebook*/
         mCallbackManager.onActivityResult(requestCode, resultCode, data);
     }
 
     /*google*/
-    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
-        try {
-            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-            //登入成功更新UI
-            updateUI(account);
-        } catch (ApiException e) {
-            Log.w(TAG, "signInResult:failed code=" + e.getStatusCode());
-            //登入失敗更新UI
-            updateUI(null);
-        }
-    }
-
-    /*google*/
-    private void updateUI(GoogleSignInAccount account) {
-        TextView tv_name = (TextView)findViewById(R.id.tvName);
-        TextView tv_email = (TextView)findViewById(R.id.tvEmail);
-        TextView tv_oauthId = (TextView)findViewById(R.id.tvOauthId);
-        ImageView iv_picture = (ImageView)findViewById(R.id.ivPicture);
-
+    private void updateGoogleInfo(GoogleSignInAccount account) {
         if (account != null){
             String name = account.getDisplayName();
             String email = account.getEmail();
-            String oauthId = account.getId();
+            String id = account.getId();
             String picture = String.valueOf(account.getPhotoUrl());
             if (account.getPhotoUrl() == null){
                 picture = "https://lh3.googleusercontent.com/-XdUIqdMkCWA/AAAAAAAAAAI/AAAAAAAAAAA/4252rscbv5M/photo.jpg";
             }
+            updateUI(id, name, email, picture, "GOOGLE");
+        }else {
+            updateUI("reset","reset","reset","reset", "GOOGLE");
+        }
+    }
+    /*facebook*/
+    private void updateFacebookInfo(AccessToken accessToken) {
+        if (accessToken != null) {
+            GraphRequest request = GraphRequest.newMeRequest(accessToken, new GraphRequest.GraphJSONObjectCallback() {
+                @Override
+                public void onCompleted(JSONObject object, GraphResponse response) {
+                    try {
+                        //String id = object.getString("id");
+                        String id = Profile.getCurrentProfile().getId();
+                        //String name = object.getString("name");
+                        String name = Profile.getCurrentProfile().getName();
+                        String email = object.getString("email");
+                        String picture = String.valueOf(Profile.getCurrentProfile().getProfilePictureUri(100, 100));
+                        updateUI(id, name, email, picture, "FACEBOOK");
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
 
+            Bundle parameters = new Bundle();
+            parameters.putString("fields", "id,name,email");
+            request.setParameters(parameters);
+            request.executeAsync();
+
+        }else {
+            updateUI("reset","reset","reset","reset","FACEBOOK");
+        }
+    }
+    /*UI*/
+    private void updateUI(String id, String name, String email, String picture, String type) {
+        if (id.equals("reset")){
+            if (oauth.equals(type)){
+                tv_oauthId.setText("id");
+                tv_name.setText("name");
+                tv_email.setText("email");
+                iv_picture.setImageResource(R.mipmap.ic_launcher_round);
+                oauth = "GUEST";
+            }
+        }else{
+            tv_oauthId.setText(id);
             tv_name.setText(name);
             tv_email.setText(email);
-            tv_oauthId.setText(oauthId);
             new DownloadImageTask(iv_picture).execute(picture);
-        }else {
-            tv_name.setText("name");
-            tv_email.setText("email");
-            tv_oauthId.setText("id");
-            iv_picture.setImageResource(R.mipmap.ic_launcher_round);
+            oauth = type;
         }
-
     }
-
-
-
-
 
     /*facebook*/
     public void printhashkey(){
